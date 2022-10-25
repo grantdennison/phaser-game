94838d930e90175a0280b35158ebe0c7648e2ffc
Grant Dennison <grantdennison@yahoo.co.uk>
2022-10-25 22:40:27 +0100

    first commit

---
+++ Preloader.js
@@ -0,0 +1,16 @@
+export default class Preloader extends Phaser.Scene {
+  constructor() {
+    super(`Preloader`);
+  }
+}
+console.log(`preloader`);
+function preload() {
+  this.load.image(`sky`, `assets/sky.png`);
+  this.load.image(`ground`, `assets/platform.png`);
+  this.load.image(`star`, `assets/star.png`);
+  this.load.image(`bomb`, `assets/bomb.png`);
+  this.load.spritesheet(`dude`, `assets/dude.png`, {
+    frameWidth: 32,
+    frameHeight: 48,
+  });
+}
